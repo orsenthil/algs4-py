048b4409a9c1beb625288a9e11de823e1bd81798
Senthil Kumaran <senthil@uthcode.com>
2019-03-29 19:55:43 -0700

    Index Min PQ.

--- algs4/IndexMinPQ.py
+++ algs4/IndexMinPQ.py
@@ -39,8 +39,11 @@ class IndexMinPQ:
     def __init__(self, maxN):
         if maxN < 0:
             raise IllegalArgumentException()
-        self.maxN = maxN       # maximum number of elements on PQ.
-        self.n = 0          # number of elements on PQ.
-        self.pq = []        # binary heap using 1 based indexing
-        self.qp = []        # inverse of pq - qp[pq[i]] = pq[qp[i]] = i
-        self.keys = []      # keys[i] = priority of i
+        self.maxN = maxN        # maximum number of elements on PQ.
+        self.n = 0              # number of elements on PQ.
+        self.pq = []            # binary heap using 1 based indexing
+        self.qp = []            # inverse of pq - qp[pq[i]] = pq[qp[i]] = i
+        self.keys = []          # keys[i] = priority of i
+
+        for i in range(maxN):
+            self.qp.append(-1)
