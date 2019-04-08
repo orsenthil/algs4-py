0404ef67e62bae965785844885fb45f0b1555ec2
Senthil Kumaran <senthil@uthcode.com>
2019-04-07 21:08:10 -0700

    Added Index MinPQ.

--- algs4/IndexMinPQ.py
+++ algs4/IndexMinPQ.py
@@ -106,6 +106,16 @@ class IndexMinPQ:
 
         return self.pq[1]
 
+    def minKey(self):
+        """Returns a minimum key.
+
+        :return: a minimum key
+        """
+        if self.n == 0:
+            raise NoSuchElementException("Priority Queue Underflow")
+        return self.keys[self.pq[1]]
+
+
     # General Helper Functions
 
     def greater(self, i, j):
