472c2fd618ad65a8aa1e55bb824816431910fb51
Senthil Kumaran <senthil@uthcode.com>
2019-04-10 21:25:09 -0700

    Updated IndexMinPQ for changeKey.

--- algs4/IndexMinPQ.py
+++ algs4/IndexMinPQ.py
@@ -145,6 +145,23 @@ class IndexMinPQ:
         else:
             return self.keys[i]
 
+    def changeKey(self, i, key):
+        """Change the key associated with index {@code i} to the specified value.
+
+        :param i: The index of the key to change.
+        :param key: the key associated with the index i to this key.
+        :return:
+        """
+        if i < 0 or i >= self.maxN:
+            raise IllegalArgumentException()
+
+        if not self.contains(i):
+            raise NoSuchElementException("Index is not in the priority queue.")
+
+        self.keys[i] = key
+        self.swim(self.qp[i])
+        self.sink(self.qp[i])
+
 
     # General Helper Functions
 
