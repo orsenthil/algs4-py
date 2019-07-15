e4119e5441d16f8eb3f92d987ea72b5d7e48f47f
Senthil Kumaran <senthil@uthcode.com>
2019-07-14 22:21:49 -0700

    increase key.

--- algs4/IndexMultiwayMinPQ.py
+++ algs4/IndexMultiwayMinPQ.py
@@ -168,6 +168,30 @@ class IndexMultiwayMinPQ:
         self.keys[i] = key
         self.swim(self.qp[i])
 
+    def increaseKey(self, i, key):
+        """ Increase the key associated with index {@code i} to the specified value.
+
+        :param i: The index of the key to increase.
+        :param key: Increase the key associated with the index to this key.
+
+        * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
+        * @throws IllegalArgumentException if {@code key <= keyOf(i)}
+        * @throws NoSuchElementException no key is associated with index {@code i}
+
+        :return: None
+        """
+        if i < 0 or i >= self.minN:
+            raise IllegalArgumentException()
+
+        if not self.contains(i):
+            raise NoSuchElementException("index is not in the priority queue.")
+
+        if self.keys[i] > key:
+            raise IllegalArgumentException("Calling increaseKey() with given argument would not strictly increase the key")
+
+        self.keys[i] = key
+        self.sink(self.qp[i])
+
     # General Helper Functions
 
     def greater(self, i, j):
