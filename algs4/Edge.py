213e99c584714754a0fbeccf4740e38d1602a1d2
Senthil Kumaran <orsenthil@gmail.com>
2019-12-28 22:04:19 -0800

    Edge data type.

--- algs4/Edge.py
+++ algs4/Edge.py
@@ -1,14 +1,7 @@
 """
-The {@code Edge} class represents a weighted edge in an
-{@link EdgeWeightedGraph}. Each edge consists of two integers
-(naming the two vertices) and a real-value weight. The data type
-provides methods for accessing the two endpoints of the edge and
-the weight. The natural order for this data type is by
-ascending order of weight.
-<p>
-For additional documentation, see <a href="https://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
-<i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
-
+The Edge class represents a weighted edge in an EdgeWeightedGraph. Each edge consists of two integers
+(naming the two vertices) and a real-value weight. The data type provides methods for accessing the two endpoints of
+the edge and the weight. The natural order for this data type is by ascending order of weight.
 """
 
 
