c2bc924f96b01e6fbe0034e59ae1928ab2a69e86
Senthil Kumaran <orsenthil@gmail.com>
2020-01-02 21:32:53 -0800

    Bag class.

--- algs4/Bag.py
+++ algs4/Bag.py
@@ -1,16 +1,14 @@
 """
-The {@code Bag} class represents a bag (or multiset) of
-generic items. It supports insertion and iterating over the
+The Bag class represents a bag (or multiset) of generic items. It supports insertion and iterating over the
 items in arbitrary order.
 
-<p>
 This implementation uses a singly linked list with a static nested class Node.
-See {@link LinkedBag} for the version from the
-textbook that uses a non-static nested class.
+See {@link LinkedBag} for the version from the textbook that uses a non-static nested class.
 See {@link ResizingArrayBag} for a version that uses a resizing array.
-The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
-take constant time. Iteration takes time proportional to the number of items.
-<p>
+
+The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations take constant time. Iteration takes time proportional
+to the number of items.
+
 For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
 <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
 """
