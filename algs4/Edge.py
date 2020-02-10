e7bb60506523057d560c0328ade61fe86d32ab5f
Senthil Kumaran <orsenthil@gmail.com>
2020-02-09 20:57:38 -0800

    Added Edge.

--- algs4/Edge.py
+++ algs4/Edge.py
@@ -1,7 +1,8 @@
 """
-The Edge class represents a weighted edge in an EdgeWeightedGraph. Each edge consists of two integers
-(naming the two vertices) and a real-value weight. The data type provides methods for accessing the two endpoints of
-the edge and the weight. The natural order for this data type is by ascending order of weight.
+The Edge class represents a weighted edge in an EdgeWeightedGraph. Each edge
+consists of two integers (naming the two vertices) and a real-value weight. The
+data type provides methods for accessing the two endpoints of the edge and the
+weight. The natural order for this data type is by ascending order of weight.
 """
 
 
@@ -31,11 +32,13 @@ class Edge:
         return self.v
 
     def other(self, vertex):
-        """Returns the endpoint of this edge that is different from the given vertex.
+        """Returns the endpoint of this edge that is different from the given
+        vertex.
 
         :param vertex: vertex one endpoint of this edge
         :return: the other endpoint of this edge
-        :throws: IllegalArgumentException if the vertex is not one of the endpoints of this edge
+        :throws: IllegalArgumentException if the vertex is not one of the
+        endpoints of this edge
         """
         if vertex == self.v:
             return self.w
@@ -44,16 +47,24 @@ class Edge:
         raise IllegalArgumentException("Illegal Endpoint")
 
     def __eq__(self, other):
-        return (self.v, self.w, self.weight) == (other.v, other.w, other.weight)
+        return (self.v, self.w, self.weight) == (other.v,
+                                                 other.w,
+                                                 other.weight)
 
     def __ne__(self, other):
-        return not (self.v, self.w, self.weight) == (other.v, other.w, other.weight)
+        return not (self.v, self.w, self.weight) == (other.v,
+                                                     other.w,
+                                                     other.weight)
 
     def __lt__(self, other):
-        return (self.v, self.w, self.weight) < (other.v, other.w, other.weight)
+        return (self.v, self.w, self.weight) < (other.v,
+                                                other.w,
+                                                other.weight)
 
     def __str__(self):
-        return "Edge({v}, {w}, {weight})".format(v=self.v, w=self.w, weight=self.weight)
+        return "Edge({v}, {w}, {weight})".format(v=self.v,
+                                                 w=self.w,
+                                                 weight=self.weight)
 
 
 if __name__ == '__main__':
